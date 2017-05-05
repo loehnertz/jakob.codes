a4d1d086dc65751e6b3ec8ee58eed4e1b31a69bc
loehnertz <jakob2102@gmail.com>
2017-05-05 23:26:15 +0200

    Renamed the main JavaScript file and fixed the 'smooth scrolling' (Source: https://css-tricks.com/snippets/jquery/smooth-scrolling/)

--- themes/jakob.codes/source/js/main.js
+++ themes/jakob.codes/source/js/main.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top - $(".nav").height() + 21
-                }, 1234);
-                return false;
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1234, function() {
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        return false;
+                    } else {
+                        $target.attr('tabindex','-1');
+                        $target.focus();
+                    }
+                });
             }
         }
     });
